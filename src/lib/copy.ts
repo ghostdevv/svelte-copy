c55fd036604a1e1ff0add702fd2a5f3b3ecd98b5
GHOSt <ghostdevbusiness@gmail.com>
2022-03-27 11:38:17 +0100

    fix: fallback to traditional execCommand if can't use better api
    Closes #90

--- src/lib/copy.ts
+++ src/lib/copy.ts
@@ -1,5 +1,37 @@
+export const copyText = async (text: string): Promise<void> => {
+    if ('ClipboardItem' in window) {
+        const blob = new Blob([text], { type: 'text/plain' });
+
+        const item = new ClipboardItem({
+            [blob.type]: blob,
+        });
+
+        await navigator.clipboard.write([item]);
+    } else {
+        const element = document.createElement('input');
+
+        element.type = 'text';
+        element.disabled = true;
+
+        element.style.setProperty('position', 'fixed');
+        element.style.setProperty('z-index', '-100');
+        element.style.setProperty('pointer-events', 'none');
+        element.style.setProperty('opacity', '0');
+
+        element.value = text;
+
+        document.body.appendChild(element);
+
+        element.click();
+        element.select();
+        document.execCommand('copy');
+
+        document.body.removeChild(element);
+    }
+};
+
 export const copy = (element: HTMLElement, text: string) => {
-    const click = async () => {
+    async function click() {
         if (text)
             try {
                 await copyText(text);
@@ -12,7 +44,7 @@ export const copy = (element: HTMLElement, text: string) => {
                     new CustomEvent('svelte-copy:error', { detail: e }),
                 );
             }
-    };
+    }
 
     element.addEventListener('click', click, true);
 
