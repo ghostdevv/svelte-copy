50231be9efad8a6da821ba1c654a1bedd7db1ace
GHOST <ghostdevbusiness@gmail.com>
2022-12-19 15:14:34 +0000

    testing

--- src/lib/copy.ts
+++ src/lib/copy.ts
@@ -1,30 +1,31 @@
+function legacyCopyText(text: string) {
+    const element = document.createElement('input');
+
+    element.type = 'text';
+    element.disabled = true;
+
+    element.style.setProperty('position', 'fixed');
+    element.style.setProperty('z-index', '-100');
+    element.style.setProperty('pointer-events', 'none');
+    element.style.setProperty('opacity', '0');
+
+    element.value = text;
+
+    document.body.appendChild(element);
+
+    element.click();
+    element.select();
+    document.execCommand('copy');
+
+    document.body.removeChild(element);
+}
+
 export const copyText = async (text: string): Promise<void> => {
-    if ('clipboard' in navigator) {
-        await navigator.clipboard.writeText(text);
-    } else {
-        /**
-         * This is the fallback deprecated way of copying text to the clipboard. Only runs if it can't find the clipboard API.
-         */
-        const element = document.createElement('input');
-
-        element.type = 'text';
-        element.disabled = true;
-
-        element.style.setProperty('position', 'fixed');
-        element.style.setProperty('z-index', '-100');
-        element.style.setProperty('pointer-events', 'none');
-        element.style.setProperty('opacity', '0');
-
-        element.value = text;
-
-        document.body.appendChild(element);
-
-        element.click();
-        element.select();
-        document.execCommand('copy');
-
-        document.body.removeChild(element);
-    }
+    // if ('clipboard' in navigator) {
+    //     await navigator.clipboard.writeText(text);
+    // } else {
+    legacyCopyText(text);
+    // }
 };
 
 interface Parameters {
